0afc4efa053876b6fa8395e264aacaebb4fe301d
Hannah Lepper <hannah.c.lepper@gmail.com>
2025-04-19 18:43:07 +0100

    fix resistance frequency discrepancy; update to model

--- fittingfunctions.jl
+++ fittingfunctions.jl
@@ -1,48 +1,19 @@
-#For beta
-function runmodelbeta(param)
-    p[1] = param[1]
-    u0 = [0.001,0.001,0.0,0.0]
+function runmodel(params)
+    p[1] = params[1]
+    p[4] = params[2]
+    u0 = [0.1,0.1,0.0,0.0]
     tspan = (0.,5000.)
-    prob = ODEProblem(tc, u0, tspan, p, dtmax = 10.)
+    prob = ODEProblem(tc, u0, tspan, p, dtmax = 1.)
     sol = solve(prob)
     sol(tspan[2])
 end
 
-function getmetricsbeta(param)
-    out = runmodelbeta(log.(param))
-    sum(out) 
+function getmetrics(param)
+    out = runmodel(log.(param))
+    [sum(out), sum(out[[2,4]])/sum(out)] #prevalence and res frequency
 end
 
-function getdistbeta(param)
-    metric = getmetricsbeta(param)
-    ((prevtarg - metric)^2)/prevtarg
+function getdist(param)
+    metrics = getmetrics(param)
+    (((prevtarg - metrics[1])^2)/prevtarg) + ((restarg - metrics[2])^2)/restarg
 end
-
-#For rest cost
-function runmodelrescost(params)
-    #p[1] = params[1]
-    p[4] = params[1]
-    u0 = [0.001,0.001,0.0,0.0]
-    tspan = (0.,5000.)
-    prob = ODEProblem(tc, u0, tspan, p, dtmax = 10.)
-    sol = solve(prob)
-    sol(tspan[2])
-end
-
-function getmetricsrescost(params)
-    out = runmodelrescost(params ./ scale_param)
-    #prev = sum(out)
-    if sum(out[[2,4]]) > 0.
-        rfreq = sum(out[[2,4]])/sum(out)
-    else
-        rfreq = 0
-    end
-     
-    return rfreq #[prev,rfreq]
-end
-
-function getdistrescost(params)
-    metrics = getmetricsrescost(params)
-    100*(restarg - metrics)^2/restarg #+ exp(((prevtarg - metrics[1])^2)/prevtarg)
-end
-
